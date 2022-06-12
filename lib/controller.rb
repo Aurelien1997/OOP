ed31b6411d8c8b2c9525c825c22d19e974e25f68
Aurelien1997 <tawpo36@gmail.com>
2022-06-12 22:56:48 +0100

    empty method

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,19 +2,8 @@ require 'item'
 require 'view'
 
 class Controller 
-
     def  consult_item
         items = Item.all
         View.all(items)
-    end
-
- 
-    def  create
-        # rentrer les attributs ici
-    end
-
- 
-    def  delete
-        # rentrer les attributs ici
     end
 end
