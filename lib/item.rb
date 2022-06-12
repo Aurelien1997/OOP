d690e51fed27340c89b0c76151f62e68633f45d6
Aurelien1997 <tawpo36@gmail.com>
2022-06-12 19:22:47 +0100

    display items

--- lib/item.rb
+++ lib/item.rb
@@ -1,18 +1,21 @@
 require 'csv'
 
 class Item
-    attr_accessor :id, :name 
+    attr_accessor :id, :name, :price, :quantity, :brand 
 
-    def initialize(id, name)
+    def initialize(id, name, price, quantity, brand)
         @id = id
         @name = name
+        @price = price
+        @quantity = quantity
+        @brand = brand
     end
 
     def self.all
         item_temporary = []
         all_items = []
         CSV.foreach("db/item.csv") do |row|
-        item_temporary = Item.new(row[0], row[1])
+          item_temporary = Item.new(row[0], row[1], row[2], row[3],  row[4])
           all_items << item_temporary
         end
         return all_items
--- lib/view.rb
+++ lib/view.rb
@@ -2,7 +2,7 @@ class View
   
   def self.all(all_items)
       all_items.each do |item|
-        puts "#{item.id} #{item.name}"
+        puts "#{item.id} #{item.name} #{item.brand} #{item.quantity} #{item.price}"
       end
   end
 
